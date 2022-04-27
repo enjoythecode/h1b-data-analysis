04ade20f2d6d8a073e995828a0ab38e6704f8ab4
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-04-27 15:50:24 +0200

    catch name changes of companies that were causing gaps in the plot

--- analyze.py
+++ analyze.py
@@ -1,3 +1,4 @@
+from curses.ascii import EM
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,6 +11,17 @@ NORMALIZE_COLUMN_NAME_MAPPING = {
     "Initial Denial": "Initial Denials",
     "Continuing Approval": "Continuing Approvals",
     "Continuing Denial": "Continuing Denials"
+}
+COGNIZANT_REAL = "COGNIZANT TECHNOLOGY SOLUTIONS US"
+INFOSYS_REAL = "INFOSYS LIMITED"
+TATA_REAL = "TATA CONSULTANCY SVCS LTD"
+EMPLOYER_SYNONYM = {
+    "COGNIZANT TECH SOLNS US CORP": COGNIZANT_REAL,
+    "COGNIZANT TECHNOLOGY SOLNS US CORP": COGNIZANT_REAL,
+    "COGNIZANT TECH SOLUTIONS US CORP": COGNIZANT_REAL,
+    "INFOSYS LTD": INFOSYS_REAL,
+    "INFOSYS TECHNOLOGIES LIMITED": INFOSYS_REAL,
+    "TATA CONSULTANCY SERVICES LIMITED": TATA_REAL
 }
 
 def load_csv(filename):
@@ -27,6 +39,7 @@ def load_csv(filename):
     df["Approvals"] = df["Initial Approvals"] + df["Continuing Approvals"]
     df["Denials"] = df["Initial Denials"] + df["Continuing Denials"]
 
+    df = df.replace({"Employer": EMPLOYER_SYNONYM})
     aggregation_functions = {'Approvals': 'sum', "Denials": 'sum'}
     df = df.groupby(df["Employer"]).aggregate(aggregation_functions)
     df.reset_index(inplace=True)
@@ -42,9 +55,11 @@ data_frames = [load_csv(DATA_FOLDER+data_files[i]) for i in range(len(data_files
 data_by_year = {str(year):df for (year, df) in data_frames}
 
 all_data = pd.concat([df for (year, df) in data_frames])
+all_data = all_data.dropna()
+
 
 ####################
-count_by_year = all_data.groupby("Year").sum()[:-1]
+count_by_year = all_data.groupby("Year").sum()
 count_by_year.reset_index(inplace=True)
 VISA_CAP = 85000
 count_by_year["Expected Lottery Chance"] = VISA_CAP / count_by_year["Approvals"] * 100
@@ -74,9 +89,30 @@ data_2021["Share of All Approvals"] = data_2021["Approvals"] / total_approvals_2
 
 top_10_2021 = data_2021.sort_values(by=['Approvals'],ascending=False)[["Employer", "Approvals", "Share of All Approvals"]][:10]
 top_10_2021.to_csv(RESULT_FOLDER + "top_10_employers_2021.csv")
+####################
 
-top_10_employers = top_10_2021["Employer"]
+####################
+top_employers = data_2021.sort_values(by=['Approvals'],ascending=False)["Employer"][:10]
+consultancies = ["COGNIZANT TECHNOLOGY SOLUTIONS US", "TATA CONSULTANCY SVCS LTD", "INFOSYS LIMITED", "WIPRO LIMITED",]
+fig, ax = plt.subplots(1,1, figsize=(12, 7))
+
+plot_employer_list = consultancies
+
+print(all_data[all_data["Employer"].str.contains("COGNI", na=False)].sort_values("Approvals", ascending=False)["Employer"][:20])
+
+for employer in plot_employer_list:
+    employer_data = all_data[all_data["Employer"] == employer]
+    employer_data = employer_data.sort_values("Year")
+    ax.plot(employer_data["Year"], employer_data["Approvals"], label = employer)
+ax.legend()
+fig.savefig(RESULT_FOLDER + "top_employers_across_years.png")
+
+# 
+#top_10_all_years = all_data[all_data["Employer"] in top_10_employers]
+#print(top_10_all_years)
 ####################
 
 # TODO
-# plot the number of petitions by the top 5 employers of 2022 across all years
+# plot the number of petitions by the top 5 employers of 2022 across all years 
+    # - axis labels
+    # - drop 0 values
